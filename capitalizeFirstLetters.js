ff44135585c061b286631e5040cad6007581bd7a
Romain heloise <romheloise@gmail.com>
2021-01-09 14:18:31 +0100

    passing test

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,10 +1,19 @@
 const assert = require("assert");
 
-const capitalizeFirstLetters = (sentence) => {};
+const capitalizeFirstLetters = (sentence) => {
+  return sentence
+    .split(" ")
+    .map((word) => {
+      return word.length > 0 ? word[0].toUpperCase() + word.slice(1) : "";
+    })
+    .join(" ");
+};
 
 assert.strictEqual(typeof capitalizeFirstLetters, "function");
 assert.strictEqual(capitalizeFirstLetters.length, 1);
-assert.strictEqual(capitalizeFirstLetters("i am learning TDD"), "I Am Learning TDD");
+assert.strictEqual(
+  capitalizeFirstLetters("i am learning TDD"),
+  "I Am Learning TDD"
+);
 assert.strictEqual(capitalizeFirstLetters("i"), "I");
 assert.strictEqual(capitalizeFirstLetters(""), "");
-
